bec4537c89a350c1f30afa694c741f1eb65ccf89
Jason Robinson <robinsonjason761@gmail.com>
2021-01-12 20:32:45 -0600

    Functions

---
+++ my_lambdata/my_script.py
@@ -0,0 +1,15 @@
+# my_lambdata/my_script.py
+
+import pandas
+
+from my_mod import enlarge
+
+print("HELLO WORLD")
+
+df = pandas.DataFrame({"state": ["CT", "CO", "CA", "TX"]})
+print(df.head())
+
+print("-----------------")
+x = 5
+print("NUMBER", x)
+print("ENLARGED NUMBER", enlarge(x)) # invoking our function!!
